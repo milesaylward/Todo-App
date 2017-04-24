50e792901f4dacb83d65680bfa36b4aa270331f8
Miles Aylward <miles.aylward@gmail.com>
2017-04-24 17:44:16 -0400

    add edit option

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,6 +18,20 @@ class TodosController < ApplicationController
     @todo = Todo.find(params[:id])
   end
 
+  def edit
+    @todo = Todo.find(params[:id])
+
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @Todo.update(todo_params)
+      flash[:notice] = "Successfully updated..."
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end
+  end
   private
     def todo_params
       params.require(:todo).permit(:name, :description)
